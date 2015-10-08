d2eca1364272bff3e41d7ab42dfaabcb7e381c81
Paul Martin <p.r.martin.sw@gmail.com>
2015-10-08 11:24:22 -0700

    Add bash script to clone/pull jkind and jkindRegression and then run the tests

---
+++ jkr.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+clear
+echo Simple Build and Test Script
+
+curdir=$PWD
+printf '%s\n' $curdir
+
+if [ -d "jkindRegression" ]; then
+   cd jkindRegression
+   git pull
+   cd ..
+else
+   git clone https://github.com/pr-martin/jkindRegression.git jkindRegression
+fi
+
+if [ -d "jkind" ]; then
+   cd jkind
+   git pull
+   cd ..
+else
+   git clone https://github.com/pr-martin/jkind.git jkind
+fi
+
+cd ./jkind
+ant clean
+ant all
+cd ..
+
+echo "Executing jkind Regression Test...Please be patient"
+cd ./jkindRegression
+python jkindtest.py -dir ../jkind/testing -jar ../jkind/build/jkind.jar -logfile ../jkr.log --gui --recur
+
+if [ $? == 0 ]; then
+   echo "Test Successful"
+else
+   echo "Test Failed, see jkr.log"
+fi
