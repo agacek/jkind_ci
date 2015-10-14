56e7fcf88fff06f491a95db2a88d58da68be788c
Paul Martin <p.r.martin.sw@gmail.com>
2015-10-14 10:42:02 -0700

    Rename jkr.sh to jkr_all.sh because it clones and builds. Add jkr_clone.sh (clone only) for use with Jenkins.

---
+++ jkr_clone.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear
+echo Simple Build and Test Script
+
+curdir=$PWD
+printf '%s\n' $curdir
+
+if [ -d "jkindRegression" ]; then
+   cd jkindRegression
+   git pull
+   cd ..
+else
+   git clone https://github.com/pr-martin/jkindRegression.git jkindRegression
+fi
+
+if [ -d "jkind" ]; then
+   cd jkind
+   git pull
+   cd ..
+else
+   git clone https://github.com/agacek/jkind.git jkind
+fi
